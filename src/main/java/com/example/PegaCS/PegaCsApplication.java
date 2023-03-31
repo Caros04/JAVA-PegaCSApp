dda697ff6eb45167888416ff267de801b1e2a32e
Caros04 <jowk04@gmail.com>
2023-03-31 10:39:07 +0700

    first setup

---
+++ src/main/java/com/example/PegaCS/PegaCsApplication.java
@@ -0,0 +1,13 @@
+package com.example.PegaCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegaCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegaCsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PegaCS/PegaCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PegaCS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegaCsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1da1205294c89862286809527c8292e2501c6e04
unknown <julius.oscar@banksinarmas.com>
2023-04-26 08:18:01 +0700

    wrong commit

--- src/main/java/com/example/PegaCS/controller/nasabahController.java
+++ src/main/java/com/example/PegaCS/controller/nasabahController.java
@@ -2,6 +2,7 @@ package com.example.PegaCS.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.example.PegaCS.repository.NasabahRepository;
 @RestController
 @RequestMapping(value = "/api")
 public class nasabahController {
+
     private final NasabahRepository nasabahRepository;
 
     public nasabahController(NasabahRepository nasabahRepository) {
@@ -22,7 +24,6 @@ public class nasabahController {
     @GetMapping("/greeting")
     public List<nasabah> getNasabah() {
         return nasabahRepository.findAlltoTen();
-        // return userRepository.findAll();
     }
 
 }
